29bbd7d08e9d5e1c76a21e13164d82b399250e59
Githubmagali <106199984+Githubmagali@users.noreply.github.com>
2023-06-10 10:31:56 -0300

    first commit

---
+++ alerts.js
@@ -0,0 +1,10 @@
+
+
+const ingresar=()=>{
+    const accesoPermitido=confirm('Cumples con todos los requisitos?');
+    if(accesoPermitido){
+                alert('Bienvenido')
+    }else{
+        alert('Lo sentimos.');
+    }
+};
