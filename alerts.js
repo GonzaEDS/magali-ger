7e916e023d6f2e63d105b406e1d77b66cf70cf32
Githubmagali <106199984+Githubmagali@users.noreply.github.com>
2023-06-12 17:18:46 -0300

    cambios

--- alerts.js
+++ alerts.js
@@ -1,10 +1,12 @@
 
 
-const ingresar=()=>{
+const ingresar00=()=>{
     const accesoPermitido=confirm('Cumples con todos los requisitos?');
     if(accesoPermitido){
-                alert('Bienvenido')
+                alert('Lo sentimos, esta pagina esta en desarrollo por lo que no se puede acceder')
     }else{
         alert('Lo sentimos.');
     }
 };
+
+
--- funciones.js
+++ funciones.js
@@ -40,6 +40,4 @@ function completarDatos(){
         }
     
       
-        
-    
     completarDatos();
